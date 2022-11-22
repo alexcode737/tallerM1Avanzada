b9486bebbe8e4de4f7074b7d626640cdcdbc632d
alexcode737 <alexis15torres16zorrilla@gmail.com>
2022-11-22 16:02:12 -0500

    se crean los archivos para cada punto del taller y se soluciona el primero

---
+++ TallerM1Avazada/punto1.js
@@ -0,0 +1,9 @@
+function hallarDistancia(x,y){
+    return Math.sqrt(((Math.pow(Math.pow(x,2)-Math.pow(x,1),2))+(Math.pow(Math.pow(y,2)-Math.pow(y,1)),2)));
+}
+
+console.log(hallarDistancia(5,6))
+
+const flechaHallarDistancia = (x,y)=>  Math.sqrt(((Math.pow(Math.pow(x,2)-Math.pow(x,1),2))+(Math.pow(Math.pow(y,2)-Math.pow(y,1)),2)));
+
+console.log(flechaHallarDistancia(5,6))
